6d19f7ad93c8b609d4194585f0ec500196696cb5
JuanSebastianGB <3393@holbertonschool.com>
2022-05-13 12:45:42 +0000

    Setting up backend workflow with docker compose

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,11 @@
 require('dotenv').config();
 
 module.exports = {
-  username: 'root',
-  password: 'root',
-  database: 'testdb',
-  host: 'localhost',
-  dialect: 'mysql',
+  username: process.env.MYSQLDB_USER || 'root',
+  password: process.env.MYSQLDB_ROOT_PASSWORD || 'root',
+  database: process.env.MYSQLDB_DATABASE || 'testdb',
+  host: process.env.MYSQLDB_HOST || 'localhost',
+  dialect: process.env.MYSQLDB_DIALECT || 'mysql',
   define: {
     timestamps: false,
     underscore: true
