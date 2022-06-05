0be1a42d2ef6b105a11a4f081cae3dc8e717fe54
JuanSebastianGB <3393@holbertonschool.com>
2022-06-04 19:14:35 -0500

    Startin a basic project structure

---
+++ frontend/src/components/sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import './sidebar.scss';
+const Sidebar = () => {
+	return (
+		<div className='sidebar'>
+			<div className='top'>
+				<div className='logo'>Eva</div>
+			</div>
+			<div className='center'>
+				<ul>
+					<li>
+						<span>Dashboard</span>
+					</li>
+					<li>
+						<span>Dashboard</span>
+					</li>
+					<li>
+						<span>Dashboard</span>
+					</li>
+					<li>
+						<span>Dashboard</span>
+					</li>
+				</ul>
+			</div>
+			<div className='bottom'>Color Options</div>
+		</div>
+	);
+};
+
+export default Sidebar;
