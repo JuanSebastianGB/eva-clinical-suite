8f41c1707075c0d068f8a2525e89eeb4a21d5069
JuanSebastianGB <3393@holbertonschool.com>
2022-06-05 10:04:08 -0500

    Adding featured component

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,4 +1,5 @@
 import Chart from '../../components/chart/Chart';
+import Featured from '../../components/featured/Featured';
 import Navbar from '../../components/navbar/Navbar';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Widget from '../../components/widget/Widget';
@@ -16,6 +17,7 @@ const Home = () => {
 					<Widget type='earning' />
 				</div>
 				<div className='charts'>
+					<Featured />
 					<Chart title='Last 6 Months (Revenue)' aspect={2 / 1} />
 				</div>
 			</div>
